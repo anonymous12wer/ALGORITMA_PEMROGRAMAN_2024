ecd768f45b1ef8df8bd53d031f073ceb1d48c2fc
anonymous12wer <fahrizalprasetya284@gmail.com>
2024-10-20 19:39:47 +0800

    Add files via upload
    Fahrizal Prasetya_2409076025

---
+++ nilai rata rata 1.2.py
@@ -0,0 +1,6 @@
+a = float(input("masukkan nilai uts = "))
+b = float(input("masukkan nilai tugas = "))
+c = float(input("masukkan nilai uas = "))
+hasil = a + b + c
+hasil = hasil / 3 
+print( "rata-rata nilai = ", hasil)
